87d6eb6e5da8a5cf9e4a7eda5660548ce23a3f6d
DaarkMoon <daarkmoon@mailoo.org>
2013-08-22 18:44:23 +0200

    New

--- Old/Code_Fent/global_values.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-
-VALUE_LIST = ("7","8","9","10","Valet","Dame","Roi","As") 
-COLOR_LIST = ("carreau", "pique", "trefle", "coeur")
-POINTS = (0,0,0,10,2,3,4,11)
-POINTS_ATOUTS = (0,0,14,10,20,3,4,11)
-CONTRACT_VALUES = (80, 90, 100, 110, 120, 130, 140, 150, 160, "capot", "generale")
